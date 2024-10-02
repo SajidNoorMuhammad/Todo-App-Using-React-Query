4c8d7bbb2d82a8400d90d21adffbe47aad58b8f6
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-02 15:11:20 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { fetchTodos } from './Utils/todos'
+import { data } from 'autoprefixer'
 
 function App() {
 
+  const {
+    data: todo = [],
+    isLoading,
+    isError } = useQuery({
+      queryKey: ["todos"],
+      queryFn: fetchTodos,
+    });
+
+  if (isError)
+    return (
+      <h1>
+        Sorry We Can't Fetch Todos
+      </h1>
+    )
+
   return (
     <>
+      <h1>
+        Todo App In React Query
+      </h1>
+      {isLoading && <h1>Loading...</h1>}
       
+      {
+        todo.map((data, ind) => (
+          <div key={ind}>
+            <h1>{data}</h1>
+          </div>
+        )
+        )
+      }
     </>
   )
 }
